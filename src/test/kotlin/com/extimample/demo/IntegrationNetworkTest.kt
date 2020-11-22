676d05441102d40c30ac2aa2ee646850280e8f79
Maerov Timofey Pavlovich <t.p.maerov@tinkoff.ru>
2020-11-22 23:49:17 +0300

    add test with TestContainer

--- src/test/kotlin/com/extimample/demo/IntegrationNetworkTest.kt
+++ src/test/kotlin/com/extimample/demo/IntegrationNetworkTest.kt
@@ -3,29 +3,35 @@ package com.extimample.demo
 import io.kotest.core.spec.style.FeatureSpec
 import io.kotest.extensions.testcontainers.perSpec
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.Network.newNetwork
 import org.testcontainers.containers.PostgreSQLContainer
 
-class IntegrationTest : FeatureSpec({
+class IntegrationNetworkTest : FeatureSpec({
+
+    val network = newNetwork()
 
     val redis = GenericContainer<Nothing>("redis:5.0.3-alpine").apply {
-        withExposedPorts(6379)
+        withNetwork (network)
+        withNetworkAliases("redis")
         start()
     }
     listener(redis.perSpec()) //converts container to listener and registering it with Kotest.
 
     val postgre = PostgreSQLContainer<Nothing>("postgres:11.1").apply {
+        withNetwork (network)
+        withNetworkAliases("postgre")
         start()
     }
     listener(postgre.perSpec()) //converts container to listener and registering it with Kotest.
 
-    val redisHost = redis.containerIpAddress + ":" + redis.firstMappedPort
     val image = System.getProperty("image", "demo:local")
     val mockProxy = GenericContainer<Nothing>(image).apply {
-        withEnv("spring.r2dbc.url", postgre.jdbcUrl)
+        withNetwork (network)
+        withEnv("spring.r2dbc.url", "r2dbc:postgresql://"+postgre.networkAliases.first()+":6379")
         withEnv("spring.r2dbc.username", postgre.username)
         withEnv("spring.r2dbc.password", postgre.password)
-
-        withEnv("datasources.redis.nodes", redisHost)
+        withEnv("datasources.redis.nodes", redis.networkAliases.first()+":6379")
+        withExposedPorts(8080)
         dependsOn(redis, postgre)
         start()
     }
@@ -34,8 +40,6 @@ class IntegrationTest : FeatureSpec({
 
     feature("get a default mock") {
         scenario("demo") {
-
-            println(redisHost)
 
             Thread.sleep(20000)
 
